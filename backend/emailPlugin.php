7fbd118cdee01235ccc9e80ef26812bde3c781c2
gmcglinn <loss4names@gmail.com>
2021-04-04 21:09:29 -0400

    email method finished, needs to be tested

--- backend/emailPlugin.php
+++ backend/emailPlugin.php
@@ -1,58 +1,43 @@
-<html>
-   
-
-
-
-   <body>
-     
-      <?php
-
-
-      //accept these through session/post variables? SQL active user var?
-         $to = ""; //Intake value
-         $subject = "This is subject"; //Based on location of usage
-         
-         $message = "<b>This is HTML message.</b>"; //Body of text, fill in from form?
-         /*
+<?php
+    /*
+        Notes on where to insert this method so far:
         Form Accepted
         Message Received
         Form Completed
         Account Created
         User Prompted for Response
+        On Approval per step
+
+        Emails that result from the messaging system will be labeled
+            from CRC@cs.newpaltz.edu still, but will contain message details
          */
 
 
-         $message .= "<h1>This is headline.</h1>"; //unsure what this is for yet
-         
+    /*
+     * Sending email from the cs server (Email recipient, email subject line, email content)
+     * Will print to console success or failure
+     * Email content is in HTML, provide html formatted text to "$content"
+     */
+    function sendMail($recipient, $subjectLine, $content){
+
+        $email_to = $recipient; //intake user to email here
+
+        $subject = $subjectLine; //email subject, changes dpeending
+
+        $message = $content; //written message of email
+
+        $headers = "From: Career Resource Center <CRC@cs.newpaltz.edu>\r\n"; //"From header", needs to have a capital "F"
+        $headers .= "MIME-Version: 1.0\r\n"; //mime version, RFC 822 compliant email header version. This is required
+        $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n"; //also a required piece, defines character set to be used in email?
+        
+        
+        if(mail($email_to, $subject, $message, $headers)) //if email sent successfully, mail() returns true value
+            echo 'successfully sent email';
+        else
+            echo 'failure to send email';
+    }
 
 
-         $header = "From:CRC@cs.newpaltz.edu \r\n";
-
-
-         $header .= "MIME-Version: 1.0\r\n";
-         $header .= "Content-type: text/html\r\n";
-         
-         $retval = mail ($to,$subject,$message,$header); //mail.php may be a better alternative to mime
-         
-
-
-
-
-
-
-
-        //check if emailer sent
-         if( $retval == true ) {
-            echo "Message sent successfully...";
-         }else {
-            echo "Message could not be sent...";
-         }
-
-
-         //return to previous session if POST used, otherwise just implement me when needed
-      ?>
-     
-   </body>
-</html>
+?>
 
 
