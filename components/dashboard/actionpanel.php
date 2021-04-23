9fd30a144b846835784b95dea7f8160b7868c9d6
Ariana <carrasqa1@wyvern.acsl.newpaltz.edu>
2021-04-22 22:38:58 -0400

    rouing for secretary and other users complete

--- components/dashboard/actionpanel.php
+++ components/dashboard/actionpanel.php
@@ -64,6 +64,42 @@ panels are contained in this file and printed based on the user type.
         </div>
 <?php
     }
+    else if (($_SESSION['user_type'] == $GLOBALS['secretary_type'])){
+?>
+        <div class="w3-row-padding w3-margin-bottom">
+            <div class="w3-quarter" onClick="location.href = './dashboard.php?content=messages'">
+                <div class="w3-container w3-red w3-padding-16">
+                    <div class="w3-left"><i class="fa fa-comment w3-xxxlarge"></i></div>
+                    <div class="w3-clear"></div>
+                    <h4>Messages</h4>
+                </div>
+            </div>
+            <div class="w3-quarter" onClick="location.href = './dashboard.php?content=search'">
+                <div class="w3-container w3-blue w3-padding-16">
+                    <div class="w3-left"><i class="fa fa-search w3-xxxlarge"></i></div>
+                    <div class="w3-clear"></div>
+                    <h4>Search</h4>
+                </div>
+            </div>
+    
+            <div class="w3-quarter" onClick="location.href = './dashboard.php?content=files'">
+                <div class="w3-container w3-orange w3-text-white w3-padding-16">
+                    <div class="w3-left"><i class="fa fa-files-o w3-xxxlarge"></i></div>
+                    <div class="w3-clear"></div>
+                    <h4>Files</h4>
+                </div>
+            </div>
+
+            <div class="w3-quarter" onClick="location.href = './dashboard.php?content=workflows'">
+                <div class="w3-container w3-teal w3-padding-16">
+                    <div class="w3-left"><i class="fa fa-share-alt w3-xxxlarge"></i></div>
+                    <div class="w3-clear"></div>
+                    <h4>Workflows</h4>
+                </div>
+            </div>
+        </div>
+<?php
+    }
     else {
 ?>
         <!-- All other users see the same action panel. -->
@@ -91,31 +127,6 @@ panels are contained in this file and printed based on the user type.
                     <h4>Workflows</h4>
                 </div>
             </div>
-
-
-
-            <!--More Bandaid Solution stuff, sorry other semesters-->
-            <div class="w3-quarter" onClick="location.href = './dashboard.php?content=forms'">
-                <div class="w3-container w3-teal w3-padding-16">
-                    <div class="w3-left"><i class="fa fa-share-alt w3-xxxlarge"></i></div>
-                    <div class="w3-clear"></div>
-                    <h4>Forms</h4>
-                </div>
-            </div>
-
-
-            <!--More Bandaid Solution stuff, sorry other semesters-->
-            <div class="w3-quarter" onClick="location.href = './dashboard.php?content=courses'">
-                <div class="w3-container w3-teal w3-padding-16">
-                    <div class="w3-left"><i class="fa fa-share-alt w3-xxxlarge"></i></div>
-                    <div class="w3-clear"></div>
-                    <h4>Courses</h4>
-                </div>
-            </div>
-    
-
-
-            
 
             <div class="w3-quarter" onClick="location.href = './dashboard.php?content=files'">
                 <div class="w3-container w3-orange w3-text-white w3-padding-16">
