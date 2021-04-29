474d126c61507556e3af50cfc87cef315c402374
gmcglinn <loss4names@gmail.com>
2021-04-29 16:58:45 -0400

    fine comb fixes to routing

--- components/dashboard/actionpanel.php
+++ components/dashboard/actionpanel.php
@@ -11,32 +11,35 @@ panels are contained in this file and printed based on the user type.
 ?>
         <!-- Currently, only the admin has a unique action panel. -->
         <div class="w3-row-padding w3-margin-bottom">
-            <div class="w3-quarter" onClick="location.href = './dashboard.php?content=messages'">
-                <div class="w3-container w3-red w3-padding-16">
-                    <div class="w3-left"><i class="fa fa-comment w3-xxxlarge"></i></div>
+        <div class="w3-quarter" onClick="location.href = './dashboard.php?content=adminTools'">
+                <div class="w3-container w3-amber w3-text-white w3-padding-16">
+                    <div class="w3-left"><i class="fa fa-diamond w3-xxxlarge"></i></div>
                     <div class="w3-clear"></div>
-                    <h4>Messages</h4>
+                    <h4>Admin Tools</h4>
                 </div>
             </div>
-            <!-- <div class="w3-quarter" onClick="location.href = './dashboard.php?content=search'">
-                <div class="w3-container w3-blue w3-padding-16">
+
+            <div class="w3-quarter" onClick="location.href = './dashboard.php?content=search'">
+                <div class="w3-container w3-blue w3-text-white w3-padding-16">
                     <div class="w3-left"><i class="fa fa-search w3-xxxlarge"></i></div>
                     <div class="w3-clear"></div>
                     <h4>Search</h4>
                 </div>
-            </div> -->
-            <!-- <div class="w3-quarter" onClick="location.href = './dashboard.php?content=create'">
-                <div class="w3-container w3-teal w3-padding-16">
-                    <div class="w3-left"><i class="fa fa-plus w3-xxxlarge"></i></div>
+            </div>
+
+            <div class="w3-quarter" onClick="location.href = './dashboard.php?content=messages'">
+                <div class="w3-container w3-red w3-padding-16">
+                    <div class="w3-left"><i class="fa fa-comment w3-xxxlarge"></i></div>
                     <div class="w3-clear"></div>
-                    <h4>Create</h4>
+                    <h4>Courses</h4>
                 </div>
-            </div> -->
+            </div>
+            
             <div class="w3-quarter" onClick="location.href = './dashboard.php?content=files'">
                 <div class="w3-container w3-orange w3-text-white w3-padding-16">
                     <div class="w3-left"><i class="fa fa-files-o w3-xxxlarge"></i></div>
                     <div class="w3-clear"></div>
-                    <h4>Files</h4>
+                    <h4>Users</h4>
                 </div>
             </div>
 
